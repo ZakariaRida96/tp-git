838964e9daef3db7038f6b837110c4c8455a1af5
zakaria ridadarajat <ZRIDADARAJAT@wideagency.com>
2023-04-26 17:45:21 +0200

    update file

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
+import numpy as np
+import pandas as pd
+
 x = 1
 
 list = []
+
+
+ar = np.array([[1.1, 2, 3.3, 4], [2.7, 10, 5.4, 7], [5.3, 9, 1.5, 15]])
+df = pd.DataFrame(ar, index=["a1", "a2", "a3"], columns=["A", "B", "C", "D"])
+
+
+print("Hey")
+print("yo")
